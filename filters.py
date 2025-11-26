84ecddb5fed3072240972d073b0039711d670f44
Edgelik <113557308+kirill9114z@users.noreply.github.com>
2025-11-26 19:28:44 +0300

    Update filters.py

--- filters.py
+++ filters.py
@@ -3,6 +3,7 @@ import time
 import pandas as pd
 import ccxt.async_support as ccxt
 from utils import retry_sleep, now_ts
+import pandas_ta as ta
 
 logger = logging.getLogger("filters")
 
@@ -13,11 +14,13 @@ async def fetch_ohlcv(exchange, pair, timeframe, limit=200):
     except Exception as e:
         if "bybit does not have market symbol" in str(e):
             return False
+
+
 async def compute_ema_deviation_from_ohlcv(exchange, pair, timeframe, ema1):
     try:
         ohlcv = await fetch_ohlcv(exchange, pair, timeframe, limit=5000)
         if ohlcv == False:
-            pair2 = f"{pair}:USDT"
+            pair2 = pair.split(":")[0]
             ohlcv = await fetch_ohlcv(exchange, pair2, timeframe, limit=5000)
 
         df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
@@ -66,7 +69,7 @@ async def fetch_24h_volume_usd(exchange, pair):
     except Exception as e:
         if "bybit does not have market symbol" in str(e):
             return False
-        logger.exception("Volume fetch failed for %s: %s", pair, e)
+        # logger.exception("Volume fetch failed for %s: %s", pair, e)
         return 0.0
 
 async def one_sma_filter(
@@ -91,7 +94,7 @@ async def one_sma_filter(
 
         # Убедимся, что у нас достаточно данных
         if len(df) < 11 or pd.isna(df['volume_sma9'].iloc[-1]):
-            print("Недостаточно данных для расчета SMA9")
+            # print("Недостаточно данных для расчета SMA9")
             return False, None
 
         # Получаем свечи
@@ -113,6 +116,18 @@ async def one_sma_filter(
     except Exception as e:
         print(f"Ошибка в check_volume_sma_filter: {e}")
         return False, None
+
+
+async def rsi_check(exchange, pair, timfraim):
+    ohlcv = await fetch_ohlcv(exchange, pair, timfraim, 5000)
+    df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+    df['close'] = df['close'].astype(float)
+
+    # Расчет RSI с помощью pandas_ta
+    df['rsi'] = ta.rsi(df['close'], length=14)
+    current_rsi = df['rsi'].iloc[-1]
+    return current_rsi
+
 async def check_volume_sma_filter(
             timeframe: str,
             symbol: str,
@@ -160,17 +175,17 @@ async def check_volume_sma_filter(
             current_candle_green = current_candle['close'] >= current_candle['open']
             prev_candle_green = prev_candle['close'] >= prev_candle['open']
             same_color = current_candle_green == prev_candle_green
-
-            print(f'\nТЕКУЩАЯ СВЕЧА:')
-            print(f'  Объем: {current_candle["volume"]:.2f}')
-            print(f'  SMA9: {current_sma9:.2f}')
-            print(f'  Процент: {current_volume_percent:.2f}%')
-            print(f'ПРЕДЫДУЩАЯ СВЕЧА:')
-            print(f'  Объем: {prev_candle["volume"]:.2f}')
-            print(f'  SMA9: {prev_sma9:.2f}')
-            print(f'  Процент: {prev_volume_percent:.2f}%')
-            print(
-                f'Цвета: текущая - {"зеленая" if current_candle_green else "красная"}, предыдущая - {"зеленая" if prev_candle_green else "красная"}')
+            #
+            # print(f'\nТЕКУЩАЯ СВЕЧА:')
+            # print(f'  Объем: {current_candle["volume"]:.2f}')
+            # print(f'  SMA9: {current_sma9:.2f}')
+            # print(f'  Процент: {current_volume_percent:.2f}%')
+            # print(f'ПРЕДЫДУЩАЯ СВЕЧА:')
+            # print(f'  Объем: {prev_candle["volume"]:.2f}')
+            # print(f'  SMA9: {prev_sma9:.2f}')
+            # print(f'  Процент: {prev_volume_percent:.2f}%')
+            # print(
+            #     f'Цвета: текущая - {"зеленая" if current_candle_green else "красная"}, предыдущая - {"зеленая" if prev_candle_green else "красная"}')
 
             # Проверяем все условия
             conditions_met = all([
@@ -213,7 +228,7 @@ async def check_volume_sma_filter(
             print(f"Ошибка в check_volume_sma_filter: {e}")
             return False, None
 
-async def passes_all_filters(pair, timeframe_cfg, min_volume_usd, exchange, is_AB, alltime=None, lst_all=None, colour=None, power = None, ema_timfraim=None, i=1):
+async def passes_all_filters(pair, timeframe_cfg, min_volume_usd, exchange, is_AB, alltime=None, lst_all=None, colour=None, power = None, ema_timfraim=None, rsi_cfg=None,i=1):
     """
     timeframe_cfg = {"EMA1":10, "EMA2":50, "threshold_pct":2.5}
     """
@@ -229,12 +244,12 @@ async def passes_all_filters(pair, timeframe_cfg, min_volume_usd, exchange, is_A
                 else:
                     ema_res = await compute_ema_deviation_from_ohlcv(exchange, pair2, timeframe_cfg['timefraim'],
                                                                      timeframe_cfg['EMA1'])
-                    print(f"EMA2 {timeframe_cfg['timefraim']}: {ema_res} {vol} {pair2}")
+                    # print(f"EMA2 {timeframe_cfg['timefraim']}: {ema_res} {vol} {pair2}")
                     if ema_res is None:
                         return False, {"reason": "ema_failed"}
                     if ema_res["deviation_pct"] < timeframe_cfg['threshold_pct']:
-                        logger.info("%s rejected by ema deviation: %.2f%% < %.2f%%", pair2, ema_res["deviation_pct"],
-                                    timeframe_cfg['threshold_pct'])
+                        # logger.info("%s rejected by ema deviation: %.2f%% < %.2f%%", pair2, ema_res["deviation_pct"],
+                        #             timeframe_cfg['threshold_pct'])
                         return False, {"reason": "ema_threshold", "deviation": ema_res["deviation_pct"]}
                     else:
                         return True, {"volume": vol, "ema": ema_res}
@@ -244,13 +259,15 @@ async def passes_all_filters(pair, timeframe_cfg, min_volume_usd, exchange, is_A
             if vol < min_volume_usd:
                 return False
         ema_res = await compute_ema_deviation_from_ohlcv(exchange, pair, timeframe_cfg['timefraim'],timeframe_cfg['EMA1'])
-        print(f"EMA {timeframe_cfg['timefraim']}: {ema_res} {vol}")
+        # print(f"EMA {timeframe_cfg['timefraim']}: {ema_res} {vol}")
         if ema_res is None:
             return False, {"reason":"ema_failed"}
         if ema_res["deviation_pct"] < timeframe_cfg['threshold_pct']:
-            logger.info("%s rejected by ema deviation: %.2f%% < %.2f%%", pair, ema_res["deviation_pct"], timeframe_cfg['threshold_pct'])
+            # logger.info("%s rejected by ema deviation: %.2f%% < %.2f%%", pair, ema_res["deviation_pct"], timeframe_cfg['threshold_pct'])
             return False, {"reason":"ema_threshold", "deviation": ema_res["deviation_pct"]}
-
+        rsi = await rsi_check(exchange, pair, rsi_cfg['timeframe'])
+        if rsi < rsi_cfg['treshold']:
+            return False, {"vol": 0, "ema": 0}
         return True, {"volume": vol, "ema": ema_res}
     else:
         if vol == False:
@@ -274,27 +291,29 @@ async def passes_all_filters(pair, timeframe_cfg, min_volume_usd, exchange, is_A
             if float(lst_res[0]['volume_percent']) <= power or lst_res[0]['candle_color'] != colour:
                 f = False
             if f:
-                print(f'ПРОШЕЛ SMA9: {pair} {lst_res} {power}')
+                # print(f'ПРОШЕЛ SMA9: {pair} {lst_res} {power}')
                 historical_results = []
                 for offset, expected_color in enumerate(lst_all, start=2):
                     historical_info = await one_sma_filter(candles, df, offset)
                     if historical_info is None:
-                        print(f"Не удалось получить данные для свечи {offset}")
+                        # print(f"Не удалось получить данные для свечи {offset}")
                         return False
                     historical_results.append(historical_info)
 
                 # Сравниваем реальные цвета с ожидаемыми из lst_all
                 all_match = all(historical_results[i]['candle_color'] == lst_all[i]for i in range(len(lst_all)))
-
+                # print(f'Pair: {pair}: {all_match}')
                 if all_match:
                     # Сделать здесь проверку на ema(так же сделать обработку ema в конфиге и импорт в main файле, чтобы закинуть сюда потом это
                     ema_res = await compute_ema_deviation_from_ohlcv(exchange, pair, ema_timfraim['timefraim'],
                                                                      ema_timfraim['EMA1'])
-                    # print(f"EMA {timeframe_cfg['timefraim']}: {ema_res} {vol}")
                     if ema_res is None:
                         return False
                     if ema_res["deviation_pct"] < ema_timfraim['threshold_pct']:
                         logger.info("%s rejected by ema deviation: %.2f%% < %.2f%%", pair, ema_res["deviation_pct"], ema_timfraim['threshold_pct'])
+                        return False
+                    rsi = await rsi_check(exchange, pair, rsi_cfg['timeframe'])
+                    if rsi < rsi_cfg['treshold']:
                         return False
                     return True
                 else:
@@ -308,7 +327,7 @@ async def passes_all_filters(pair, timeframe_cfg, min_volume_usd, exchange, is_A
         except Exception as e:
             if i == 1:
                 pair2 = pair.split(':')[0]
-                await passes_all_filters(pair2, timeframe_cfg, min_volume_usd, exchange, is_AB, alltime, lst_all, colour, power, 2)
+                await passes_all_filters(pair2, timeframe_cfg, min_volume_usd, exchange, is_AB, alltime, lst_all, colour, power, ema_timfraim, rsi_cfg,2)
             if "bybit does not have market symbol" in str(e) and i == 2:
                 return False
 
