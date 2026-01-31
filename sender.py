cacc8a979d3de6c8209f91c111dfa84bd89889bb
Edgelik <113557308+kirill9114z@users.noreply.github.com>
2026-01-31 15:08:04 +0300

    Update sender.py

--- sender.py
+++ sender.py
@@ -4,6 +4,10 @@ from utils import now_ts
 from aiogram.client.default import DefaultBotProperties
 logger = logging.getLogger("sender")
 
+green_circle = '\U0001F7E2'
+red_circle = '\U0001F534'
+
+
 class Sender:
     def __init__(self, bot_token, storage):
         self.bot = Bot(token=bot_token, default=DefaultBotProperties(parse_mode="HTML"))
@@ -13,23 +17,53 @@ class Sender:
         global text
         chat_id = chat_cfg
         # dedup check
-        last = self.storage.get_last_sent(chat_id, pair)
         now = now_ts()
-        if last and (now - int(last) < send_duplicate_seconds):
-            logger.info("Skipping duplicate for %s to chat %s (last %s sec ago)", pair, chat_id, now - int(last))
-            return False
-        if is_ab:
-            text = self.format_message(pair, payload)
+
+        # ДЕДУПЛИКАЦИЯ В ЗАВИСИМОСТИ ОТ ТИПА ГРУППЫ
+        if is_ab == "Three":  # группы 5-6 с Pivot
+            # извлекаем метку и таймфрейм из payload
+            pivot_level = payload.get('metka')  # R1, S2, PP
+            pivot_timeframe = payload.get('metla_tf')  # 5m, 1h, 1d
+
+            if not pivot_level or not pivot_timeframe:
+                logger.error("Pivot signal missing level/timeframe: %s", payload)
+                return False
+
+            # проверяем дедупликацию по pivot-ключу
+            last = self.storage.get_last_sent_pivot(chat_id, pair, pivot_level, pivot_timeframe)
+            if last and (now - int(last) < send_duplicate_seconds):
+                # logger.info("Skipping duplicate Pivot %s %s:%s for chat %s",
+                #             pair, pivot_level, pivot_timeframe, chat_id)
+                return False
+
+            text = self.format_text_pivot(pair, payload)
+
+            try:
+                await self.bot.send_message(chat_id, text)
+                self.storage.set_last_sent_pivot(chat_id, pair, pivot_level, pivot_timeframe, now)
+                # logger.info("Sent Pivot signal %s %s:%s to %s",
+                #             pair, pivot_level, pivot_timeframe, chat_id)
+                return True
+            except Exception as e:
+                logger.exception("Failed to send Pivot message to %s: %s", chat_id, e)
+                return False
         else:
-            text = self.format_text2(pair, payload)
-        try:
-            await self.bot.send_message(chat_id, text)
-            self.storage.set_last_sent(chat_id, pair, now)
-            logger.info("Sent signal %s to %s", pair, chat_id)
-            return True
-        except Exception as e:
-            logger.exception("Failed to send message to %s: %s", chat_id, e)
-            return False
+            last = self.storage.get_last_sent(chat_id, pair)
+            if last and (now - int(last) < send_duplicate_seconds):
+                # logger.info("Skipping duplicate for %s to chat %s (last %s sec ago)", pair, chat_id, now - int(last))
+                return False
+            if is_ab:
+                text = self.format_message(pair, payload)
+            else:
+                text = self.format_text2_group34(pair, payload)
+            try:
+                await self.bot.send_message(chat_id, text)
+                self.storage.set_last_sent(chat_id, pair, now)
+                # logger.info("Sent signal %s to %s", pair, chat_id)
+                return True
+            except Exception as e:
+                logger.exception("Failed to send message to %s: %s", chat_id, e)
+                return False
 
     def format_message(self, pair, payload):
         coin = pair.split('/')[0]
@@ -43,28 +77,48 @@ class Sender:
         if str(vol)[-1] == "0":
             vol = int(vol)
         circle = payload['circle']
-        side = "+" if circle == "🟢" else ""
+        side = "+" if circle == f"{green_circle}" else ""
 
         time3 = payload["3time"]
         circle3 = payload["3circle"]
         vol3 = payload["3vol"]
         return (f"{circle} <code>{coin}</code> {side}{vol}%\n"
-                f"EMA{ema_vol} {timefraim} 🟢 +{int(ema.get('deviation_pct'))}%\n"
+                f"EMA{ema_vol} {timefraim} {green_circle} +{int(ema.get('deviation_pct'))}%\n"
                 f"{time3} {circle3}{vol3}%")
 
-    def format_text2(self, pair, payload):
+    def format_text2_group34(self, pair, payload):
         coin = pair.split('/')[0]
         circle = payload['circle']
         vol = payload['val']
         if str(vol)[-1] == "0":
             vol = int(vol)
-        side = "+" if circle == "🟢" else ""
-        second_sicrle = payload['circle3'] * payload['all_time']
+        res = [f"{green_circle}" if i == "BUY" else f"{red_circle}" for i in payload["lst_alltime"]]
+        result = ''.join(res)
+        side = "+" if circle == f"{green_circle}" else ""
         secind_tim = payload['timefraim']
         tresh_day = payload['trech_day']
         cirlce_day = payload['circle_day']
         return (f"{circle} <code>{coin}</code> {side}{vol}%\n"
-                f"{second_sicrle} {secind_tim}\n"
+                f"{result}{green_circle if payload['first_cirle'] == '' else red_circle} {secind_tim}\n"
                 f"1D {cirlce_day}{tresh_day}%")
+
+    def format_text_pivot(self, pair, payload):
+        coin = pair.split('/')[0]
+        circle = payload['circle']
+        val = payload['val']
+        side = "+" if circle == f"{green_circle}" else ""
+
+        metka = payload['metka']
+        metka_tf = payload['metka_tf']
+        metka_vol = payload['metka_vol']
+        sign = payload['metka_sign']
+
+        circle2 = payload['circle2']
+        time = payload["time"]
+        globla_thres = payload['globla_thres']
+        return (f"{circle} <code>{coin}</code> {side}{val}%\n"
+                f"{metka} {metka_tf} {metka_vol}% {sign}\n"
+                f"{time} {circle2}{globla_thres}%"
+                )
 
 
