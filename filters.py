e91d41c2e93c67d15669c63693d51bb6f0cae177
Edgelik <113557308+kirill9114z@users.noreply.github.com>
2026-01-31 15:07:29 +0300

    Update filters.py

--- filters.py
+++ filters.py
@@ -3,9 +3,66 @@ import time
 import pandas as pd
 import ccxt.async_support as ccxt
 from utils import retry_sleep, now_ts
-import pandas_ta as ta
+# import pandas_ta as ta
+import pandas_ta_classic as ta
+from typing import Dict
 
 logger = logging.getLogger("filters")
+
+ALL_PIVOT_LEVELS = ['S5', 'S4', 'S3', 'S2', 'S1', 'PP', 'R1', 'R2', 'R3', 'R4', 'R5']
+R_LEVEL = ['R1', 'R2', 'R3', 'R4', 'R5']
+S_LEVEL = ['S5', 'S4', 'S3', 'S2', 'S1']
+AUTO_TIMEFRAME_MAP = {
+        '1s': '1d', '5s': '1d', '15s': '1d', '30s': '1d',
+        '1m': '1d', '3m': '1d', '5m': '1d', '15m': '1d', '30m': '1d',
+        '1h': '1d', '2h': '1d', '4h': '1d', '6h': '1d', '12h': '1d',
+        '1d': '1w',
+        '1w': '1M',
+        '1M': '3M',
+    }
+
+TIMEFRAIM_CHART = {
+        "5m": "1h",
+        "15m": "1h",
+        "30m": "1d",
+        "1h": "1d",
+        "4h": "1d",
+        "1d": "1w",
+    }
+
+
+def calculate_pivot_points(high: float, low: float, close: float) -> Dict[str, float]:
+    pp = (high + low + close) / 3
+    range_value = high - low
+
+    # базовые уровни (совпадают с UI)
+    r1 = (2 * pp) - low
+    r2 = pp + range_value
+    r3 = high + 2 * (pp - low)
+
+    s1 = (2 * pp) - high
+    s2 = pp - range_value
+    s3 = low - 2 * (high - pp)
+
+    # ВАЖНО: расширение уровней 4/5 как на UI (равный шаг)
+    dr = r3 - r2  # шаг вверх
+    ds = s2 - s3  # шаг вниз
+
+    r4 = r3 + dr
+    r5 = r4 + dr
+
+    s4 = s3 - ds
+    s5 = s4 - ds
+
+    return {
+        'PP': pp,
+        'R1': r1, 'R2': r2, 'R3': r3, 'R4': r4, 'R5': r5,
+        'S1': s1, 'S2': s2, 'S3': s3, 'S4': s4, 'S5': s5,
+        'range': range_value,
+        'source_high': high,
+        'source_low': low,
+        'source_close': close
+    }
 
 @retry_sleep(tries=3)
 async def fetch_ohlcv(exchange, pair, timeframe, limit=200):
@@ -114,7 +171,6 @@ async def one_sma_filter(
 
 
     except Exception as e:
-        print(f"Ошибка в check_volume_sma_filter: {e}")
         return False, None
 
 
@@ -228,7 +284,199 @@ async def check_volume_sma_filter(
             print(f"Ошибка в check_volume_sma_filter: {e}")
             return False, None
 
-async def passes_all_filters(pair, timeframe_cfg, min_volume_usd, exchange, is_AB, alltime=None, lst_all=None, colour=None, power = None, ema_timfraim=None, rsi_cfg=None,i=1):
+
+async def check_level(exchange,
+               symbol: str,
+               target_level: str,
+               pivot_timeframe: str,
+               sign: str,
+               threshold_min: float = -1.0,
+               threshold_max: float = 1.0,
+               ) -> Dict:
+    if target_level not in ALL_PIVOT_LEVELS:
+        return {
+            'success': False,
+            'error': f'Неверная метка. Доступные: {", ".join(ALL_PIVOT_LEVELS)}'
+        }
+
+    try:
+        # Определяем таймфрейм для расчета
+
+        # Получаем последние 2 свечи для расчета Pivot
+        ohlcv = await exchange.fetch_ohlcv(
+            symbol=symbol,
+            timeframe=pivot_timeframe,
+            limit=2
+        )
+
+        if len(ohlcv) < 2:
+            return {
+                'success': False,
+                'error': 'Недостаточно данных для расчета Pivot'
+            }
+
+        # Рассчитываем Pivot на основе ПРЕДЫДУЩЕЙ завершенной свечи
+        prev_candle = ohlcv[-2]
+        high = prev_candle[2]
+        low = prev_candle[3]
+        close = prev_candle[4]
+
+        pivots = calculate_pivot_points(high, low, close)
+        # print(f'\n\n\nPIVOTS: {pivots}\n\n\n')
+        # Получаем текущую цену (close текущей незакрытой свечи)
+        current_price = ohlcv[-1][4]
+
+        if current_price is None:
+            return {
+                'success': False,
+                'error': 'Не удалось получить текущую цену'
+            }
+
+        # Получаем значение целевого уровня
+        level_value = pivots[target_level]
+
+        # Рассчитываем отклонение
+        deviation = ((current_price - level_value) / level_value) * 100
+        if level_value < 0:
+            deviation *= -1
+        if sign == '+':
+            in_range = threshold_min <= deviation
+        elif sign == '-':
+            in_range = threshold_max >= deviation
+        else:
+            in_range = threshold_min <= deviation <= threshold_max
+        return {
+            'success': True,
+            'pass_filter': in_range,
+            'symbol': symbol,
+            'target_level': target_level,
+            'level_value': level_value,
+            'current_price': current_price,
+            'deviation_percent': round(deviation, 4),
+            'threshold_min': threshold_min,
+            'threshold_max': threshold_max,
+            'in_range': in_range,
+            'all_pivots': pivots
+        }
+
+    except Exception as e:
+        return {
+            'success': False,
+            'error': f'Ошибка: {str(e)}'
+        }
+
+
+async def pivot_allchack(exchange, pair, lst_conf):
+    valid_levels = []
+    SOM = {
+        "5m": "1d",
+        "15m": "1d",
+        "30m": "1w",
+        "1h": "1w",
+        "4h": "1w",
+        "1d": "1M"
+    }
+    for level_name, config in lst_conf.items():
+        if level_name not in ALL_PIVOT_LEVELS:
+            print(f"⚠️ Неверная метка: {level_name}")
+            continue
+
+        timeframe = config.get('time', '')
+        thres = config.get('thres', 0)
+
+        if timeframe not in AUTO_TIMEFRAME_MAP:
+            print(f"⚠️ Неверный таймфрейм для {level_name}: {timeframe}")
+            continue
+
+        valid_levels.append({
+            'level': level_name,
+            'timeframe': timeframe,
+            'thres': thres
+        })
+
+    if not valid_levels:
+        print("❌ Нет валидных меток для проверки")
+        return []
+
+    # Группируем по таймфреймам
+    tf_groups = {}
+    for level_info in valid_levels:
+        tf = level_info['timeframe']
+        if tf not in tf_groups:
+            tf_groups[tf] = []
+        tf_groups[tf].append(level_info)
+
+
+    # Результаты
+    passed_levels = []
+    lst_success_tf = []
+    # Обрабатываем каждую группу таймфреймов
+    for timeframe, levels_in_tf in tf_groups.items():
+        # print(f"\n📊 Проверяем таймфрейм: {timeframe} ({len(levels_in_tf)} меток)")
+
+        level_priority = {
+            'R5': 5, 'R4': 4, 'R3': 3, 'R2': 2, 'R1': 1,
+            'S5': 5, 'S4': 4, 'S3': 3, 'S2': 2, 'S1': 1
+        }
+
+        sorted_levels = sorted(
+            levels_in_tf,
+            key=lambda x: level_priority[x['level']],
+            reverse=True  # от старшей к младшей
+        )
+
+        lst_success_tf = []
+
+        # Проверяем от старшей к младшей
+        for level_info in sorted_levels:
+            level_name = level_info['level']
+            thres = level_info['thres']
+            sign = level_name['sign']
+            if timeframe in lst_success_tf:
+                continue
+            # print(f"  🔍 Проверяем {level_name} (thres: {thres}%)...")
+            # if level_name in R_LEVEL:
+            #     result = await check_level(
+            #         exchange,
+            #         symbol=pair,
+            #         target_level=level_name,
+            #         pivot_timeframe=SOM[timeframe],
+            #         threshold_min=-thres,  # симметричный диапазон ±thres
+            #         threshold_max=thres,
+            #     )
+            # else:
+            result = await check_level(
+                exchange,
+                symbol=pair,
+                target_level=level_name,
+                pivot_timeframe=SOM[timeframe],
+                sign=sign,
+                threshold_min=-thres,  # симметричный диапазон ±thres
+                threshold_max=thres,
+
+            )
+
+            if result['success'] and result['pass_filter']:
+                lst_success_tf.append(timeframe)
+                # print(f"    ✅ ПРОЙДЕНО! deviation: {result['deviation_percent']:+.4f}%")
+                passed_levels.append({
+                    'level': level_name,
+                    'timeframe': timeframe,
+                    'deviation_percent': result['deviation_percent'],
+                    'level_value': result['level_value'],
+                })
+                # Продолжаем проверять младшие (может быть несколько)
+            else:
+                print(f"    ❌ Не пройдено (deviation: {result.get('deviation_percent', 'N/A'):+.4f}%)")
+
+        # print(f"  ✅ В таймфрейме {timeframe}: {len([l for l in passed_levels if l['timeframe'] == timeframe])} меток пройдено")
+
+    print(f"\n🎯 ИТОГО ПРОЙДЕННЫХ МЕТОК: {len(passed_levels)}")
+    print(f'\n\n\n{passed_levels}\n\n\n')
+    return passed_levels
+
+
+async def passes_all_filters(pair, timeframe_cfg, min_volume_usd, exchange, is_AB, alltime=None, lst_all=None, colour=None, power = None, ema_timfraim=None, rsi_cfg=None, lst_cnf=None, i=1):
     """
     timeframe_cfg = {"EMA1":10, "EMA2":50, "threshold_pct":2.5}
     """
@@ -252,7 +500,13 @@ async def passes_all_filters(pair, timeframe_cfg, min_volume_usd, exchange, is_A
                         #             timeframe_cfg['threshold_pct'])
                         return False, {"reason": "ema_threshold", "deviation": ema_res["deviation_pct"]}
                     else:
-                        return True, {"volume": vol, "ema": ema_res}
+                        if rsi_cfg['RSI'] == 1:
+                            rsi = await rsi_check(exchange, pair, rsi_cfg['timefraim'])
+                            if rsi < rsi_cfg['RSI_VOL']:
+                                return False, {"vol": 0, "ema": 0}
+                            return True, {"volume": vol, "ema": ema_res}
+                        else:
+                            return True, {"volume": vol, "ema": ema_res}
             else:
                 return False, {"reason":"volume", "volume": vol}
         else:
@@ -265,10 +519,37 @@ async def passes_all_filters(pair, timeframe_cfg, min_volume_usd, exchange, is_A
         if ema_res["deviation_pct"] < timeframe_cfg['threshold_pct']:
             # logger.info("%s rejected by ema deviation: %.2f%% < %.2f%%", pair, ema_res["deviation_pct"], timeframe_cfg['threshold_pct'])
             return False, {"reason":"ema_threshold", "deviation": ema_res["deviation_pct"]}
-        rsi = await rsi_check(exchange, pair, rsi_cfg['timeframe'])
-        if rsi < rsi_cfg['treshold']:
-            return False, {"vol": 0, "ema": 0}
-        return True, {"volume": vol, "ema": ema_res}
+        if rsi_cfg['RSI'] == 1:
+            rsi = await rsi_check(exchange, pair, rsi_cfg['timefraim'])
+            if rsi < rsi_cfg['RSI_VOL']:
+                return False, {"vol": 0, "ema": 0}
+            return True, {"volume": vol, "ema": ema_res}
+        else:
+            return True, {"volume": vol, "ema": ema_res}
+
+    elif is_AB == "Three":
+        if vol == False:
+            pair = pair.split(':')[0]
+            vol = await fetch_24h_volume_usd(exchange, pair)
+            if vol:
+                if vol < min_volume_usd:
+                    return False
+        else:
+            if vol < min_volume_usd:
+                return False
+        try:
+            res = await pivot_allchack(exchange, pair, lst_cnf)
+            if res:
+                return True, res
+            else:
+                return False, {"reason": "pivot_failed"}
+        except Exception as e:
+            if i == 1:
+                pair2 = pair.split(':')[0]
+                await passes_all_filters(pair2, timeframe_cfg, min_volume_usd, exchange, is_AB, alltime, lst_all,
+                                         colour, power, ema_timfraim, rsi_cfg, lst_cnf, 2)
+            if "bybit does not have market symbol" in str(e) and i == 2:
+                return False
     else:
         if vol == False:
             pair = pair.split(':')[0]
@@ -303,6 +584,11 @@ async def passes_all_filters(pair, timeframe_cfg, min_volume_usd, exchange, is_A
                 # Сравниваем реальные цвета с ожидаемыми из lst_all
                 all_match = all(historical_results[i]['candle_color'] == lst_all[i]for i in range(len(lst_all)))
                 # print(f'Pair: {pair}: {all_match}')
+                # if rsi_cfg['RSI'] == 1:
+                #     rsi = await rsi_check(exchange, pair, rsi_cfg['timefraim'])
+                #     print(f'{pair}: {rsi} ^ {rsi_cfg['RSI_VOL']} {rsi_cfg['timefraim']}  | {is_AB}')
+                #     if rsi < rsi_cfg['RSI_VOL']:
+                #         return False
                 if all_match:
                     # Сделать здесь проверку на ema(так же сделать обработку ema в конфиге и импорт в main файле, чтобы закинуть сюда потом это
                     ema_res = await compute_ema_deviation_from_ohlcv(exchange, pair, ema_timfraim['timefraim'],
@@ -312,14 +598,16 @@ async def passes_all_filters(pair, timeframe_cfg, min_volume_usd, exchange, is_A
                     if ema_res["deviation_pct"] < ema_timfraim['threshold_pct']:
                         logger.info("%s rejected by ema deviation: %.2f%% < %.2f%%", pair, ema_res["deviation_pct"], ema_timfraim['threshold_pct'])
                         return False
-                    rsi = await rsi_check(exchange, pair, rsi_cfg['timeframe'])
-                    if rsi < rsi_cfg['treshold']:
-                        return False
-                    return True
+                    if rsi_cfg['RSI'] == 1:
+                        rsi = await rsi_check(exchange, pair, rsi_cfg['timefraim'])
+                        if rsi < rsi_cfg['RSI_VOL']:
+                            return False
+                        return True
+                    else:
+                        return True
                 else:
                     return False
             else:
-                # print(f'МЕНЬШЕ {pair}: {lst_res}')
                 return False
         # else:
         #     print(f'Не одна длина в filtrs')
@@ -327,7 +615,7 @@ async def passes_all_filters(pair, timeframe_cfg, min_volume_usd, exchange, is_A
         except Exception as e:
             if i == 1:
                 pair2 = pair.split(':')[0]
-                await passes_all_filters(pair2, timeframe_cfg, min_volume_usd, exchange, is_AB, alltime, lst_all, colour, power, ema_timfraim, rsi_cfg,2)
+                await passes_all_filters(pair2, timeframe_cfg, min_volume_usd, exchange, is_AB, alltime, lst_all, colour, power, ema_timfraim, rsi_cfg,lst_cnf, 2)
             if "bybit does not have market symbol" in str(e) and i == 2:
                 return False
 
@@ -337,10 +625,16 @@ async def main():
     exchange = ccxt.bybit({
         'enableRateLimit': True,
         })
-    t1 = time.time()
-    ok = await passes_all_filters("DOGE/USDT", '4h', 1000, exchange, False, 3, [20, 100, 1], "SELL", 1)
-    print(f'3 {ok}')
-    # print(f'RES: {t, l}\n{time.time() - t1}')
+
+    lst_conf = {
+        "R1": {"time": "1h", "thres": 1.5},
+        "R2": {"time": "1h", "thres": 0.1},
+        "R3": {"time": "30m", "thres": 0.01},
+        "R4": {"time": "1d", "thres": 1},
+        "R5": {"time": "1d", "thres": 2}
+    }
+    res = await pivot_allchack(exchange, "XAUT/USDT", lst_conf)
+    print(f'res999: {res}')
     await exchange.close()
 if __name__ == "__main__":
     asyncio.run(main())
